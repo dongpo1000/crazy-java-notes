2eacd803e43dce6953ba3314c5953b353c1e0a8b
dongpo <dongbianshanpo@163.com>
2020-12-27 21:33:26 +0800

    完成第1章Java语言概述与开发环境

---
+++ src/main/java/com/crazy/chapter01/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.crazy.chapter01;
+
+/**
+ * @Description HelloWorld
+ * 本章练习：
+ * 1.搭建自己的Java开发环境
+ * 2.编写Java语言的HelloWorld
+ * @Author DongPo
+ * @Date 2020-12 21:21
+ */
+public class HelloWorld {
+
+	/**
+	 * Java程序的入口方法，程序将从这里开始执行
+	 *
+	 * @param args 参数
+	 */
+	public static void main(String[] args) {
+		// 向控制台打印一条语句
+		System.out.println("HelloWorld!");
+	}
+}
